235b43754db83367551749a08708b1c711bc882e
LUIZINFMZ <luiz.sebrian.silva@escola.pr.gov.br>
2025-12-01 11:39:48 -0300

    ..

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que eu aprendi na programção?',
+    'aprendemos a usar o figma'
+)
+
+criaCartao(
+    'Geografia',
+    'O que eu aprendi na programção?',
+    'fazer o projeto flash cards'
+)
+
+criaCartao(
+    'Programação',
+    'O que eu aprendi na programção?',
+    'A usars o github'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que eu aprendi na programção?',
+    'google colab'
+)
